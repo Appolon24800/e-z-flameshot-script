e5beff868ba3c07909fd6d8e0e99f506f3b94c34
Appolon <93398824+Appolon24800@users.noreply.github.com>
2024-10-19 19:09:46 +0200

    Update e-z.sh

--- e-z.sh
+++ e-z.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-auth="API KEY HERE."
+auth="API_KEY"
 url="https://api.e-z.host/files"
 
 temp_file="/tmp/screenshot.png"
-flameshot gui -r > $temp_file
+grim -g "$(slurp)" - > $temp_file
 
 if [[ $(file --mime-type -b $temp_file) != "image/png" ]]; then
     rm $temp_file
@@ -15,23 +15,16 @@ image_url=$(curl -X POST -F "file=@"$temp_file -H "key: "$auth -v "$url" 2>/dev/
 echo $image_url > /tmp/upload.json
 response_file="/tmp/upload.json"
 
-if ! jq -e . >/dev/null 2>&1 < /tmp/upload.json; then
-    notify-send "Error occurred while uploading. Please try again later." -a "Flameshot"
-    rm $temp_file
-    rm $response_file
-    exit 1
-fi
-
 success=$(cat /tmp/upload.json | jq -r ".success")
 if [[ "$success" != "true" ]] || [[ "$success" == "null" ]]; then
     error=$(cat /tmp/upload.json | jq -r ".error")
     if [[ "$error" == "null" ]]; then
-        notify-send "Error occurred while uploading. Please try again later." -a "Flameshot"
+        notify-send "Error occurred while uploading. Please try again later." -a "Grim"
         rm $temp_file
         rm $response_file
         exit 1
     else
-        notify-send "Error: $error" -a "Flameshot"
+        notify-send "Error: $error" -a "Grim"
         rm $temp_file
         rm $response_file
         exit 1
@@ -39,6 +32,5 @@ if [[ "$success" != "true" ]] || [[ "$success" == "null" ]]; then
 fi
 
 cat /tmp/upload.json | jq -r ".imageUrl" | xclip -sel c
-notify-send "Image URL copied to clipboard" -a "Flameshot" -i $temp_file
+notify-send "Image URL copied to clipboard" -a "Grim" -i $temp_file
 rm $temp_file
-
